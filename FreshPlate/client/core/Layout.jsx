0f39bf774747af4ae1e007ff2919a31f6e4e342e
bsalunga-cen <bsalunga@my.centennialcollege.ca>
2024-11-08 14:22:47 -0500

    updated signin

--- FreshPlate/client/core/Layout.jsx
+++ FreshPlate/client/core/Layout.jsx
@@ -1,35 +1,79 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import auth from '../lib/auth-helper';
 import "./Layout.css";
 
+const isActive = (location, path) => {
+  return location.pathname === path ? { color: '#ff4081' } : { color: '#ffffff' };
+};
+
 export default function Layout() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <div className="app-container">
       <AppBar position="static">
         <Toolbar>
-          <Typography>
-            <Button component="a" href="/">
+          <Typography variant="h6" color="inherit" sx={{ flexGrow: 1 }}>
+            <Button component={Link} to="/" color="inherit">
               FreshPlate
             </Button>
           </Typography>
-          <Button component="a" href="/">
-            Home
-          </Button>
-          <Button component="a" href="/about">
-            My Account
-          </Button>
-          <Button component="a" href="/recipes">
+          <IconButton component={Link} to="/" color="inherit" aria-label="Home" style={isActive(location, "/")}>
+            <HomeIcon />
+          </IconButton>
+          <Button component={Link} to="/recipes" style={isActive(location, "/recipes")}>
             Recipes
           </Button>
-          <Button component="a" href="/about">
-            About Us
+          <Button component={Link} to="/users" style={isActive(location, "/users")}>
+            Users
           </Button>
-          <Button component="a" href="/contact">
-            Contact
-          </Button>
-          <Button component="a" href="/logout">
-            Logout
-          </Button>
+          {!auth.isAuthenticated() && (
+            <>
+              <Button component={Link} to="/signup" style={isActive(location, "/signup")}>
+                Sign up
+              </Button>
+              <Button component={Link} to="/signin" style={isActive(location, "/signin")}>
+                Sign In
+              </Button>
+            </>
+          )}
+          {auth.isAuthenticated() && (
+            <>
+              <Button
+                component={Link}
+                to={`/user/${auth.isAuthenticated().user._id}`}
+                style={isActive(location, `/user/${auth.isAuthenticated().user._id}`)}
+              >
+                My Profile
+              </Button>
+              <Button
+                component={Link}
+                to="/about"
+                style={isActive(location, "/about")}
+              >
+                About Us
+              </Button>
+              <Button
+                component={Link}
+                to="/contact"
+                style={isActive(location, "/contact")}
+              >
+                Contact
+              </Button>
+              <Button
+                color="inherit"
+                onClick={() => {
+                  auth.clearJWT(() => navigate('/'));
+                }}
+              >
+                Sign out
+              </Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </div>

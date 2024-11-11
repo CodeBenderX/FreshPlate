551d3fd6eac487b858616a85dfb6f3afa5b5c60c
lmenil <lmenil@my.centennialcollege.ca>
2024-11-10 21:00:24 -0500

    clean dir structures

--- FreshPlate/client/src/main.jsx
+++ FreshPlate/client/src/main.jsx
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App'; // Import the main App component
-// import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// );
-
-
-// index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';

636598e645671528efc079bbe37a4410a8e9c2f2
angelo <angelo.tiquio@outlook.com>
2024-11-10 20:32:57 -0500

    editted Layout

--- FreshPlate/client/core/Layout.jsx
+++ FreshPlate/client/core/Layout.jsx
@@ -1,3 +1,123 @@
+// import React from "react";
+// import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+// import { Link, useNavigate, useLocation } from "react-router-dom";
+// import auth from "../lib/auth-helper";
+// import "./Layout.css";
+// import logo from "../src/assets/FreshPlate-logo.png";
+
+// const isActive = (location, path) => {
+//   return location.pathname === path
+//     ? { color: "#FF6E1C" }
+//     : { color: "#000000" };
+// };
+
+// export default function Layout() {
+//   const navigate = useNavigate();
+//   const location = useLocation();
+
+//   return (
+//     <div className="app-container">
+//       <AppBar position="static">
+//         <Toolbar>
+//           <Typography variant="h6" color="#FF6E1C" sx={{ flexGrow: 1 }}>
+//             <Button component={Link} to="/" color="inherit">
+//               <img src={logo} alt="FreshPlate-Logo" height={20} />
+//             </Button>
+//           </Typography>
+//           <Button
+//             component={Link}
+//             to="/"
+//             color="inherit"
+//             style={isActive(location, "/")}
+//           >
+//             Home
+//           </Button>
+//           {/* for testing */}
+//           {/* <Button
+//             component={Link}
+//             to="/account"
+//             color="inherit"
+//             style={isActive(location, "/account")}
+//           >
+//             Account
+//           </Button> */}
+//           <Button
+//             component={Link}
+//             to="/about"
+//             color="inherit"
+//             style={isActive(location, "/about")}
+//           >
+//             About Us
+//           </Button>
+//           <Button
+//             component={Link}
+//             to="/contact"
+//             color="inherit"
+//             style={isActive(location, "/contact")}
+//           >
+//             Contact
+//           </Button>
+//           {!auth.isAuthenticated() && (
+//             <>
+//               <Button
+//                 component={Link}
+//                 to="/signin"
+//                 color="inherit"
+//                 style={isActive(location, "/signin")}
+//               >
+//                 Sign In
+//               </Button>
+//               <Button
+//                 component={Link}
+//                 to="/signup"
+//                 color="inherit"
+//                 style={isActive(location, "/signup")}
+//               >
+//                 Register
+//               </Button>
+//             </>
+//           )}
+//           {auth.isAuthenticated() && (
+//             <>
+//               <Button
+//                 component={Link}
+//                 to="/account"
+//                 color="inherit"
+//                 style={isActive(location, "/account")}
+//               >
+//                 Account
+//               {/* </Button>
+//               <Button
+//                 component={Link}
+//                 to="/about"
+//                 style={isActive(location, "/about")}
+//               >
+//                 About Us
+//               </Button>
+//               <Button
+//                 component={Link}
+//                 to="/contact"
+//                 style={isActive(location, "/contact")}
+//               >
+//                 Contact */}
+//               </Button>
+//               <Button
+//                 color="inherit"
+//                 style={isActive(location, "/logout")}
+//                 onClick={() => {
+//                   auth.clearJWT(() => navigate("/"));
+//                 }}
+//               >
+//                 Logout
+//               </Button>
+//             </>
+//           )}
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
+
 import React from "react";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { Link, useNavigate, useLocation } from "react-router-dom";
@@ -24,41 +144,32 @@ export default function Layout() {
               <img src={logo} alt="FreshPlate-Logo" height={20} />
             </Button>
           </Typography>
-          <Button
-            component={Link}
-            to="/"
-            color="inherit"
-            style={isActive(location, "/")}
-          >
-            Home
-          </Button>
-          {/* for testing */}
-          {/* <Button
-            component={Link}
-            to="/account"
-            color="inherit"
-            style={isActive(location, "/account")}
-          >
-            Account
-          </Button> */}
-          <Button
-            component={Link}
-            to="/about"
-            color="inherit"
-            style={isActive(location, "/about")}
-          >
-            About Us
-          </Button>
-          <Button
-            component={Link}
-            to="/contact"
-            color="inherit"
-            style={isActive(location, "/contact")}
-          >
-            Contact
-          </Button>
           {!auth.isAuthenticated() && (
             <>
+              <Button
+                component={Link}
+                to="/"
+                color="inherit"
+                style={isActive(location, "/")}
+              >
+                Home
+              </Button>
+              <Button
+                component={Link}
+                to="/about"
+                color="inherit"
+                style={isActive(location, "/about")}
+              >
+                About Us
+              </Button>
+              <Button
+                component={Link}
+                to="/contact"
+                color="inherit"
+                style={isActive(location, "/contact")}
+              >
+                Contact
+              </Button>
               <Button
                 component={Link}
                 to="/signin"
@@ -81,15 +192,32 @@ export default function Layout() {
             <>
               <Button
                 component={Link}
+                to="/member"
+                color="inherit"
+                style={isActive(location, "/member")}
+              >
+                Home
+              </Button>
+              <Button
+                component={Link}
                 to="/account"
                 color="inherit"
                 style={isActive(location, "/account")}
               >
                 Account
-              {/* </Button>
+              </Button>
+              <Button
+                component={Link}
+                to="/recipes"
+                color="inherit"
+                style={isActive(location, "/recipes")}
+              >
+                Recipes
+              </Button>
               <Button
                 component={Link}
                 to="/about"
+                color="inherit"
                 style={isActive(location, "/about")}
               >
                 About Us
@@ -97,9 +225,10 @@ export default function Layout() {
               <Button
                 component={Link}
                 to="/contact"
+                color="inherit"
                 style={isActive(location, "/contact")}
               >
-                Contact */}
+                Contact
               </Button>
               <Button
                 color="inherit"
